d0d56a02aa37916e913c4adc907a7811bc819a6c
Olumide04 <ayeromara12@gmail.com>
2023-07-20 21:41:07 +0000

    commit

---
+++ 0x03-log_parsing/0-generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+import sys
+from time import sleep
+import datetime
+
+for i in range(10000):
+    sleep(random.random())
+    sys.stdout.w60 HTTP/1.1\" {} {}\n".format(
+        random.randrite("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/2int(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Log parsing with metrics computation
+"""
+
+import sys
+import re
+
+if __name__ == '__main__':
+
+    line_count = 0
+    total_file_size = 0
+    status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {cods(stats: dict, total_size: int) -> None:
+        pe: 0 for code in status_codes}
+
+    def print_statrint("File size:", total_size)
+        for code in sorted(stats):
+            if stats[code] > 0:
+                print(code + ":", stats[code])
+
+    try:
+        for line in sys.stdin:
+            li        match = re.match(r'.*GET \/projects\/260 Hne_count += 1
+            line = line.strip()
+    TTP\/1\.1" (\d+) (\d+)', line)
+            if match:
+                status_code = match.group(1)
+                file_size = int(match.group(2))
+                if status_code in status_codes:
+                    stats[status_code] += 1
+                total_file_size += file_size
+            if line_count % 10 == 0:
+                print_stats(stats, total_file_size)
+        print_stats(stats, total_file_size)
+    except KeyboardInterrupt:
+        print_stats(stats, total_file_size)
+        raise
