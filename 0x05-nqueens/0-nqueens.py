6dd8b47a50b53582248fbf059c965e84dbd9af07
Olumide04 <ayeromara12@gmail.com>
2023-08-03 23:05:01 +0000

    commit

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,73 @@
 #!/usr/bin/python3
 """
-The N Queens 
-Show all possible ways to place N non-attacking Queens on an NxN board
+N queens puzzle solution
 """
 
 import sys
-import math
 
-solutions = []
-can_place_count = [0]
-nqueens_count = [0]
-
-def can_place(board, row, col):
-    """Checks if [row, col] is a safe position on the board"""
-    can_place_count[0] += 1
-    for i in range(row):
-        if board[i] == col or abs(i - row) == abs(board[i] - col):
+def is_safe(board, row, col, N):
+    """
+    Check if it's safe to place a queen at board[row][col]
+    """
+    # Check the left side of the row
+    for i in range(col):
+        if board[row][i]:
             return False
+
+    # Check upper diagonal on the left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j]:
+            return False
+
+    # Check lower diagonal on the left side
+    for i, j in zip(range(row, N), range(col, -1, -1)):
+        if board[i][j]:
+            return False
+
     return True
 
+def nqueens(N):
+    """
+    Solve the N queens puzzle and print the solutions
+    """
+    if not isinstance(N, int):
+        print("N must be a number")
+        sys.exit(1)
 
-def nqueens(board, row, n):
-    """O(n!) time, O(n) space"""
-    nqueens_count[0] += 1
-    if row == n:
-        return solutions.append(board[:])
-    for col in range(n):
-        if can_place(board, row, col):
-            board[row] = col
-            nqueens(board, row + 1, n)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for _ in range(N)] for _ in range(N)]
+
+    def solve(row):
+        """
+        Recursive backtracking function to find solutions
+        """
+        if row == N:
+            for i in range(N):
+                for j in range(N):
+                    if board[i][j]:
+                        print("[{}, {}]".format(i, j), end=" ")
+            print()
+
+        for col in range(N):
+            if is_safe(board, row, col, N):
+                board[row][col] = 1
+                solve(row + 1)
+                board[row][col] = 0
+
+    solve(0)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: nqueens N") or exit(1)
+        print("Usage: {} N".format(sys.argv[0]))
+        sys.exit(1)
+
     try:
-        n = int(sys.argv[1])
+        N = int(sys.argv[1])
+        nqueens(N)
     except ValueError:
-        print("N must be a number") or exit(1)
-    if n < 4:
-        print("N must be at least 4") or exit(1)
-    nqueens([None] * n, 0, n)
-    [print([[row, col] for row, col in enumerate(sol)]) for sol in solutions]
-    print(n, can_place_count, nqueens_count, can_place_count[0]/nqueens_count[0], math.factorial(n), can_place_count[0]/(pow(2, n) + n))
+        print("N must be a number")
+        sys.exit(1)
+
