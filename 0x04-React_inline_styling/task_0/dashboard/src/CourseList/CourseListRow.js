0dda7a15a2befe0f3e393e1118139e9b8634193e
olumide04 <ayeromara12@gmail.com>
2023-08-08 23:24:54 +0100

    commit

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,26 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const rowStyle = {
-  backgroundColor: "#f5f5f5ab",
-};
-
-const headerStyle = {
-  backgroundColor: "#deb5b545",
-};
-
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  const rowStyle = {
+    backgroundColor: isHeader ? "#deb5b545" : "#f5f5f5ab",
+  };
+
+  const headerCellStyle = {
+    backgroundColor: "#deb5b545",
+  };
+
   return (
     <tr style={rowStyle}>
       {isHeader ? (
         textSecondCell === null ? (
-          <th style={headerStyle} colSpan={2}>
-            {textFirstCell}
-          </th>
+          <th colSpan={2}>{textFirstCell}</th>
         ) : (
           <>
-            <th style={headerStyle}>{textFirstCell}</th>
-            <th style={headerStyle}>{textSecondCell}</th>
+            <th style={headerCellStyle}>{textFirstCell}</th>
+            <th>{textSecondCell}</th>
           </>
         )
       ) : (
